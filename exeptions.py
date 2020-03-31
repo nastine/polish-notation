eaed8b6d6d7c8d9de6d2b4c08288689626e6ea8d
nastine <nastine928@gmail.com>
2020-03-31 17:00:45 +0300

    Assert fixed, check for negative numbers added

--- exeptions.py
+++ exeptions.py
@@ -4,25 +4,26 @@ class LengthError(Exception):
     print(self)
 
 def polish_notation(user_list):
-  try:
-    assert user_list[0] in ["+", "-", "*", "/"], "Данной операции нет среди доступных!"
-    if len(user_list) != 3:
-      raise LengthError(len(user_list) - 1)
-    elif user_list[0] == "+":
-      print(int(user_list[1]) + int(user_list[2]))
-    elif user_list[0] == "-":
-      print(int(user_list[1]) - int(user_list[2]))
-    elif user_list[0] == "*":
-      print(int(user_list[1]) * int(user_list[2]))
-    elif user_list[0] == "/":
-      print(int(user_list[1]) / int(user_list[2]))
-  except AssertionError as error:
-    print(error)
-  except LengthError as error:
-    print(error)
-  except ZeroDivisionError:
-    print("Деление на ноль невозможно!")
-  except ValueError:
-    print("В качестве операндов введены не числа!")
+  assert user_list[0] in ["+", "-", "*", "/"], "Данной операции нет среди доступных!"
+  if int(user_list[1]) > 0 < int(user_list[2]):
+    try:
+      if len(user_list) != 3:
+        raise LengthError(len(user_list) - 1)
+      elif user_list[0] == "+":
+        print(int(user_list[1]) + int(user_list[2]))
+      elif user_list[0] == "-":
+        print(int(user_list[1]) - int(user_list[2]))
+      elif user_list[0] == "*":
+        print(int(user_list[1]) * int(user_list[2]))
+      elif user_list[0] == "/":
+        print(int(user_list[1]) / int(user_list[2]))
+    except LengthError as error:
+      print(error)
+    except ZeroDivisionError:
+      print("Деление на ноль невозможно!")
+    except ValueError:
+      print("В качестве операндов введены не числа!")
+  else:
+    print("Программа работает только с положительными числами!")
 
-polish_notation(input("Введите операцию: ").split())
+polish_notation(input("Введите операцию, разделяя символы пробелами: ").split())
